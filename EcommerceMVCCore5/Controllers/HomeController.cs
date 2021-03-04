98bce312b1b32afe2d0b394a1b83342fc2d9bae8
Prateek Saini <prateeksaini15@yahoo.com>
2021-03-04 13:54:30 +0530

    Add project files.

---
+++ EcommerceMVCCore5/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using EcommerceMVCCore5.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using EcommerceMVCCore5.Interfaces;
+using System.Globalization;
+
+namespace EcommerceMVCCore5.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private IProductRepository Repository;
+        public HomeController(ILogger<HomeController> logger, IProductRepository repo)
+        {
+            _logger = logger;
+            Repository = repo;
+        }
+
+        public IActionResult Index()
+        {
+            
+            return View(Repository.Products);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ EcommerceMVCCore5/Interfaces/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EcommerceMVCCore5.Models;
+namespace EcommerceMVCCore5.Interfaces
+{
+    public interface IProductRepository
+    {
+        IQueryable<Product> Products { get; }
+    }
+}
